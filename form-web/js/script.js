af41ac6a2605dbd558f6132ae11ddf00bc77f53b
Fabizzz <165857418+Fabiz2@users.noreply.github.com>
2026-06-03 11:48:43 -0300

    feat: adicionando novos campos

--- form-web/js/script.js
+++ form-web/js/script.js
@@ -3,7 +3,9 @@
 */
 const nome = document.querySelector("#nome");
 const button = document.querySelector("button");
-const lista = document.querySelector(".lista")
+const lista = document.querySelector(".lista");
+const email = document.querySelector("#email");
+const telefone = document.querySelector("#telefone");
 
 /* 
     Criando função
@@ -12,10 +14,16 @@ button.addEventListener("click", (event) => {
     event.preventDefault();
     
    const inputValue = nome.value;
-   const templateHTML = `<li>${inputValue}</li>`; 
+   const inputValueEmail = email.value;
+   const inputValueTelefone = telefone.value;
+
+   const templateHTML = `<li>nome: ${inputValue} <br>E-mail: ${inputValueEmail} <br>Telefone: ${inputValueTelefone}<br></li>`; 
+   
     //incluir itens no HTML
     lista.innerHTML += templateHTML;
 
     //limpando campo
     nome.value = "";
+    email.value = "";
+    telefone.value = "";
 });
