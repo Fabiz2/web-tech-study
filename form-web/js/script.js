a2c54ace877b2e01066603b04820270e87cf0043
Fabizzz <165857418+Fabiz2@users.noreply.github.com>
2026-06-03 11:15:59 -0300

    feat: finalizando o layout de css e adicionando javaScript no projeto

---
+++ form-web/js/script.js
@@ -0,0 +1,17 @@
+/*
+    Criando variaveis
+*/
+const nome = document.querySelector("#nome");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista")
+
+/* 
+    Criando função
+*/
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    console.log(nome.value)
+    //incluir itens no HTML
+    
+})
