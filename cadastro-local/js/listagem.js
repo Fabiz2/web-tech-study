28e20aac1c3dfcf008862ba0ac8970c128098bf5
Fabiz2 <1905fabriz@gmail.com>
2026-06-17 11:15:02 -0300

    feat: laços de repetição

---
+++ cadastro-local/js/listagem.js
@@ -0,0 +1,8 @@
+const listaValores = document.querySelector("#listaValores");
+
+//função para carregar valores na lista
+carregarValoreSalvos();
+
+function carregarValoreSalvos(){
+    console.log("carregarValoreSalvos");
+}
