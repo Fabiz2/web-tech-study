7c618329b5d237dc10dba3e0ebb02fe24d67323b
Fabiz2 <1905fabriz@gmail.com>
2026-06-17 10:44:25 -0300

    feat: eventos do botao salvar

---
+++ cadastro-local/js/script.js
@@ -0,0 +1,20 @@
+const campo = document.querySelector("#campo")
+const salvarBtn = document.querySelector("#salvar")
+
+//adicionar evento clique no botao salva
+salvarBtn.addEventListener("click", function(event){
+    event.preventDefault()
+    const valor = campo.value;
+
+    if(valor.trim() == ""){
+        alert("Campo obrigatorio")
+        return
+    }
+
+    //salvar no storage
+    const chave = `ValorSalvo_${Date.now()}`;
+    localStorage.setItem(chave, valor);
+
+    //limpar campo apos salvar
+    campo.value = "";
+})
